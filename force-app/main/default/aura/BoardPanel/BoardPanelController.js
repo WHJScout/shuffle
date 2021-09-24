66e1a433cc035cd1a44af646be75462937435e41
wangheng <270041282@qq.com>
2021-09-24 22:04:41 +0800

    20210924

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
-                
-    }
+    startGame : function(component, event, helper) {
+        let gameModeComboBox = component.find("gameMode");
+        let value = gameModeComboBox.get("v.value");
+        console.log(value);
+        console.log(111);
+        
+    },
+    reshuffleBoard : function(component, event, helper) {
+        console.log(222);
+    },
+    
 })
